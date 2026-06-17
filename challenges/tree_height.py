b3a00f34f8666ee9a8dd8b6445f7bfc2dfb2fa07
João Marcelo Cruz Coelho <joao.ccoelho@upe.br>
2026-06-17 00:01:36 -0300

    Feat: Resolução do último exercício e compatibilidade com os testes.

--- challenges/tree_height.py
+++ challenges/tree_height.py
@@ -2,4 +2,10 @@ from data_structures.node import Node
 
 
 def tree_height(root: Node | None) -> int:
-    raise NotImplementedError
+    if root is None: # caso base: árvore vazia tem altura -1
+        return -1
+
+    left_height = tree_height(root.left) # altura da subárvore esquerda
+    right_height = tree_height(root.right) # altura da subárvore direita
+
+    return max(left_height, right_height) + 1 # a altura da árvore é a maior altura das subárvores (direita e esquesda) + 1 para contar o nó atual
